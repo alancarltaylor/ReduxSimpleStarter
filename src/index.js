4631f6a5fa3bc154c0d65d0b354acca016175d24
Alan Taylor <alan.taylor@zayo.com>
2017-04-04 16:49:58 -0600

    setup youtube api, simple search bar implemented

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAEYK9rbc27VCgEdh-ZerzRpryxmzqfs8U';
+
 // Create a new component. This component should produce some html
 const App = function(){
-	return <div>Hello World of Zayo</div>;
+	return (<div>
+	<SearchBar />
+	</div>);
 }
 
 
